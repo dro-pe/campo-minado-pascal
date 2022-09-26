4f59bed9b5fc573aa5039e858dbde5353b3e553c
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2022-09-26 16:31:01 -0300

    modularization

--- campo-minado.pas
+++ campo-minado.pas
@@ -1,56 +1,78 @@
 program campo_minado_pascal;
-const MIN = 0; MAX = 101; BORDA = -1; MINA = 9;
+const MIN = 0; MAX = 51; BORDA = -1; MINA = 9;
 type status = record
-    conteudo: longint;
+    conteudo: integer;
     aberta  : boolean;
 end;
 type matriz_tabuleiro = array[MIN..MAX,MIN..MAX] of status;
 type campo_minado = record
     tabuleiro     : matriz_tabuleiro;
-    numero_linhas : longint;
-    numero_colunas: longint;
+    numero_linhas : integer;
+    numero_colunas: integer;
     falta_abrir   : longint;
 end;
 
-procedure inicia_campo(var c: campo_minado);
-var i, j, num_minas, randi, randj, num_lin, num_col: longint;
-    frac_minas                                     : real;
+procedure zera_campo(var c: campo_minado);
+var i, j: integer;
 begin
-    writeln('Digite dois inteiros de 1 a 50 (numero de linhas e colunas do campo minado):');
-    read(num_lin, num_col);
-        while((num_lin < 1) OR (num_col < 1) OR (num_lin > 50) OR (num_col > 50)) do
-        begin
-            writeln('Valores invalidos. Por favor, digite dois inteiros de 1 a 50:');
-            read(num_lin, num_col);
-        end;
-
-    c.numero_linhas := num_lin;
-    c.numero_colunas := num_col;
-
-    frac_minas := 0.2;
-    
-    num_minas := round(frac_minas*num_lin*num_col);
-    c.falta_abrir := c.numero_linhas*c.numero_colunas - num_minas;
-
-    for i := 0 to num_lin+1 do
-        for j := 0 to num_col+1 do
+    for i := 0 to c.numero_linhas+1 do
+        for j := 0 to c.numero_colunas+1 do
         begin
             c.tabuleiro[i,j].conteudo := BORDA;
             c.tabuleiro[i,j].aberta := false;
         end;
 
-    for i := 1 to num_lin do
-        for j := 1 to num_col do
+    for i := 1 to c.numero_linhas do
+        for j := 1 to c.numero_colunas do
             c.tabuleiro[i,j].conteudo := 0;
+end;
 
-    for i := 1 to num_minas do
+procedure distribui_minas(var c    : campo_minado;
+                          num_minas: integer);
+var cont, randi, randj: integer;
+begin
+    for cont := 1 to num_minas do
     begin
         repeat
-            randi := round(random(num_lin-1)) + 1;  (* os numeros aleatorios gerados devem ser 1 <= n <= tamanho da linha ou coluna *)
-            randj := round(random(num_col-1)) + 1;
-        until(c.tabuleiro[randi,randj].conteudo <> MINA); (* o repeat evita que a mina seja colocada em uma casa onde ja havia uma mina (otimizar isso depois)*)
+            randi := round(random(c.numero_linhas-1)) + 1;  (* os numeros aleatorios gerados devem ser 1 <= n <= numero de linhas ou de colunas *)
+            randj := round(random(c.numero_colunas-1)) + 1;
+        until(c.tabuleiro[randi,randj].conteudo <> MINA);   (* o repeat evita que a mina seja colocada em uma casa onde ja havia uma mina (otimizar isso depois)*)
         c.tabuleiro[randi,randj].conteudo := MINA;
     end;
+end;
+
+function conta_minas(var c   : campo_minado;
+                     lin, col: integer): integer;
+var i, j: integer;
+begin
+    conta_minas := 0;
+    for i := -1 to 1 do
+        for j := -1 to 1 do
+            if c.tabuleiro[lin + i,col + j].conteudo = MINA then
+                conta_minas := conta_minas + 1;
+end;
+
+procedure inicia_campo(var c: campo_minado);
+var num_minas, num_lin, num_col: integer;
+    frac_minas                 : real;
+begin
+    writeln('Digite dois inteiros de 1 a 50 (numero de linhas e colunas do campo minado):');
+    read(num_lin, num_col);
+    while((num_lin < 1) OR (num_col < 1) OR (num_lin > 50) OR (num_col > 50)) do
+    begin
+        writeln('Valores invalidos. Por favor, digite dois inteiros de 1 a 50:');
+        read(num_lin, num_col);
+    end;
+
+    c.numero_linhas := num_lin;
+    c.numero_colunas := num_col;
+    frac_minas := 0.2;
+    num_minas := round(frac_minas*num_lin*num_col);
+    c.falta_abrir := c.numero_linhas*c.numero_colunas - num_minas;
+    zera_campo(c);
+    distribui_minas(c, num_minas);
+
+
 end;
 
 procedure imprime_campo(var c: campo_minado);
