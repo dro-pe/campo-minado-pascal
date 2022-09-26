2f38340a6662599e78c15a0b5220f7ec1b9b150e
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2022-09-26 20:01:55 -0300

    add read play function

--- campo-minado.pas
+++ campo-minado.pas
@@ -1,15 +1,17 @@
 program campo_minado_pascal;
-const MIN = 0; MAX = 51; BORDA = -1; MINA = 9;
-type status = record
+const MIN = 1; MAX = 50; BORDA = -1; NADA = 0; MINA = 9;
+type casa = record
     conteudo: integer;
     aberta  : boolean;
 end;
-type matriz_tabuleiro = array[MIN..MAX,MIN..MAX] of status;
+type matriz_tabuleiro = array[MIN-1..MAX+1,MIN-1..MAX+1] of casa;
 type campo_minado = record
     tabuleiro  : matriz_tabuleiro;
     num_lin    : integer;
     num_col    : integer;
     falta_abrir: longint;
+    ganhou     : boolean;
+    perdeu     : boolean;
 end;
 
 procedure zera_campo(var c: campo_minado);
@@ -24,7 +26,7 @@ begin
 
     for i := 1 to c.num_lin do
         for j := 1 to c.num_col do
-            c.tabuleiro[i,j].conteudo := 0;
+            c.tabuleiro[i,j].conteudo := NADA;
 end;
 
 procedure distribui_minas(var c    : campo_minado;
@@ -56,17 +58,18 @@ procedure inicia_campo(var c: campo_minado);
 var i, j, num_minas: integer;
     frac_minas     : real;
 begin
-    writeln('Digite dois inteiros de 1 a 50 (numero de linhas e colunas do campo minado):');
+    writeln('Digite dois inteiros de ',MIN,' a ',MAX,' (numero de linhas e colunas do campo minado):');
     read(c.num_lin, c.num_col);
-    while((c.num_lin < 1) OR (c.num_col < 1) OR (c.num_lin > 50) OR (c.num_col > 50)) do
+    while((c.num_lin < MIN) OR (c.num_col < MIN) OR (c.num_lin > MAX) OR (c.num_col > MAX)) do
     begin
-        writeln('Valores invalidos. Por favor, digite dois inteiros de 1 a 50:');
+        writeln('Valores invalidos. Por favor, digite dois inteiros de ',MIN,' a ',MAX,':');
         read(c.num_lin, c.num_col);
     end;
 
-    frac_minas := 0.2;
+    frac_minas := 0.15;
     num_minas := round(frac_minas*c.num_lin*c.num_col);
     c.falta_abrir := c.num_lin*c.num_col - num_minas;
+
     zera_campo(c);
     distribui_minas(c, num_minas);
 
@@ -84,10 +87,10 @@ begin
     begin
         for j := 1 to c.num_col do
         begin
-            // if not(c.tabuleiro[i,j].aberta) then
-            //     write('* ')
-            // else
-                if c.tabuleiro[i,j].conteudo = 0 then
+            if not(c.tabuleiro[i,j].aberta) then
+                write('* ')
+            else
+                if c.tabuleiro[i,j].conteudo = NADA then
                     write('  ')
                 else if c.tabuleiro[i,j].conteudo = MINA then
                     write('@ ')
@@ -98,23 +101,51 @@ begin
     end;
 end;
 
+procedure le_jogada(x, y: integer;
+                    c   : campo_minado);
+begin
+    writeln('Digite as coordenadas da casa que voce quer abrir (numero da linha e depois numero da coluna):');
+    read(x, y);
+    while((x < 1) OR (y < 1) OR (x > c.num_lin) OR (y > c.num_col) OR c.tabuleiro[x,y].aberta) do
+    begin
+        if (c.tabuleiro[x,y].aberta) then
+        begin
+            writeln('Essa casa ja foi aberta, digite outra coordenada:');
+            read(x, y);
+        end
+        else
+        begin
+            writeln('Por favor, digite coordenadas validas:');
+            read(x, y);
+        end;
+    end;
+end;
+
+procedure executa_jogada(x, y: integer;
+                         c   : campo_minado);
+
+begin
+
+end;
+
 var c   : campo_minado;
-    // x, y: longint;
+    x, y: integer;
 begin
     randomize;
-
     inicia_campo(c);
     imprime_campo(c);
+    c.ganhou := false;
+    c.perdeu := false;
 
-    // while not(ganhou(c) or perdeu(c)) do
-    // begin
-    //     le_jogada(x, y, c);
+    while not(c.ganhou OR c.perdeu) do
+    begin
+        le_jogada(x, y, c);
     //     executa_jogada(x, y, c);
-    //     imprime_campo(c);
-    // end;
+        imprime_campo(c);
+    end;
 
-    // if ganhou(c) then
-    //     writeln('Parabens, voce ganhou!')
-    // else
-    //     writeln('MINA! Fim de jogo.');
+    if c.ganhou then
+        writeln('Parabens, voce ganhou!')
+    else
+        writeln('MINA! Fim de jogo.');
 end.
